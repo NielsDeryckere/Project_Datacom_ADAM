fdf77f2eaaac5d6ef94e415c16a8fcb4c0e0f794
Niels Deryckere <niels.deryckere@student.howest.be>
2015-04-28 10:11:18 +0200

    Succcesfull merge

---
+++ 2nmct4-Project_Adam_Module/Adam_module/ViewModel/LoginVM.cs
@@ -0,0 +1,38 @@
+﻿using GalaSoft.MvvmLight.CommandWpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Adam_module.ViewModel
+{
+    class LoginVM : ObservableObject, IPage
+    {
+        ApplicationVM appvm = App.Current.MainWindow.DataContext as ApplicationVM;
+        SmartCard.Roles cardRole;
+        
+
+        public string Name
+        {
+            get { return "First page"; }
+        }
+
+        private void ReadCard()
+        {
+            cardRole = SmartCard.ReadCard();
+            if(cardRole != SmartCard.Roles.Reset)
+            {
+                appvm.huidigeGebruiker = cardRole;
+                appvm.ChangePage(new PageOneVM());
+            }
+                
+        }
+
+        public ICommand ReadCardCommand
+        {
+            get { return new RelayCommand(ReadCard); }
+        }
+    }
+}
